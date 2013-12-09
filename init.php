06bbd56d85e889150698c20223b6b8364f5d8e2e
Dafydd Vaughan <dafydd.vaughan@digital.cabinet-office.gov.uk>
2013-12-09 18:58:41 +0000

    always use local.conf.php for config

--- init.php
+++ init.php
@@ -35,8 +35,8 @@ spl_autoload_register(function ($class) {
 require_once('util.php');
 
 require_once('conf.php');
-if (file_exists(CONF_DIR . '/' . gethostname() . '.conf.php')) {
-    require_once(gethostname() . '.conf.php');
+if (file_exists(CONF_DIR . '/local.conf.php')) {
+    require_once('local.conf.php');
 }
 
 if (!defined('LOG_DIR')) {
